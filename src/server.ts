fbca1bd794c2affc52c687c2fa750b0aa65e5302
Pedra1 <pe_lfsilva@hotmail.com>
2021-08-06 11:08:39 -0300

    Ajusts to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,4 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes/routes";
+import { app } from "./app";
 
-export const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
 
