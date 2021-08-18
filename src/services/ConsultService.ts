6e53702359560218b50670aa92fdb6e69b0e4872
Pedra1 <pe_lfsilva@hotmail.com>
2021-08-18 20:57:59 -0300

    .

--- src/services/ConsultService.ts
+++ src/services/ConsultService.ts
@@ -12,12 +12,37 @@ export default {
   async getCustumerByTelephone(telephone: string) {
     const entityManager = getManager();
 
-    const ddd = telephone.substr(0, 2)
-    const firstNumbers = telephone.length === 10 ? telephone.substr(2, 4) : telephone.substr(2, 5)
-    const lastNumbers = telephone.length === 10 ? telephone.substr(6, 9) : telephone.substr(7, 10)
+    const ddd = telephone.substr(0, 2);
+    const firstNumbers =
+      telephone.length === 10 ? telephone.substr(2, 4) : telephone.substr(2, 5);
+    const lastNumbers =
+      telephone.length === 10
+        ? telephone.substr(6, 9)
+        : telephone.substr(7, 10);
 
     return await entityManager.query(
-      `SELECT p.phone from erp.people as p WHERE p.phone like '%${ddd}%${firstNumbers}%${lastNumbers}%' `
+      `SELECT
+        p.id,
+        p.type_tx_id,
+        p.tx_id,
+        p.name,
+        p.name_2,
+        p.postal_code,
+        p.street,
+        p.street_type,
+        p.number,
+        p.address_complement,
+        p.neighborhood,
+        p.client,
+        p.email,
+        p.birth_date,
+        p.created,
+        p.modified,
+        p.phone,
+        p.cell_phone_1,
+        p.cell_phone_2,
+        p.status
+      from erp.people as p WHERE p.phone like '%${ddd}%${firstNumbers}%${lastNumbers}%' `
     );
   },
 
@@ -28,5 +53,4 @@ export default {
       `SELECT * FROM custumer WHERE cnpj like '${cnpj}' `
     );
   },
-
 };
