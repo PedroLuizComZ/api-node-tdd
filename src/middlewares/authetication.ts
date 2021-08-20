e27611fc96d07eb78a6f67e5495d26383c3c4af5
Pedra1 <pe_lfsilva@hotmail.com>
2021-08-20 20:13:06 +0000

    Error log created

--- src/middlewares/authetication.ts
+++ src/middlewares/authetication.ts
@@ -1,20 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import ErrorMessage from "src/utils/ErrorMessage";
 
 export default function (req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).send({
-      status: false,
-      error: "No token provided",
-    });
+    return ErrorMessage(res , "No token provided")
   }
 
   if (authorization !== process.env.AUTH_KEY) {
-    return res.status(401).send({
-      status: false,
-      error: "Invalid token",
-    });
+    return ErrorMessage(res , "Invalid token")
   }
 
   next();
--- src/utils/ErrorMessage.ts
+++ src/utils/ErrorMessage.ts
@@ -1,5 +1,21 @@
 import { Response } from "express";
 
+import fs from 'fs';
+
 export default function ErrorMessage(res: Response, message: string) {
+  LogError(res,message,);
   return res.status(400).send({ status: false, error: message });
 }
+
+async function LogError(res: Response, text: string) { 
+
+  await fs.readFile(__dirname + '/debug.log', function(err, buf) {
+
+    text = `${text}, ${res.req.originalUrl} , ${new Date()}`
+
+    let data = `${buf.toString()}\n${text}`
+
+    fs.writeFile(__dirname + '/debug.log', data, (err) => { if (err) console.log(err); });
+
+  });
+};
